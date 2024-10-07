7e2b9a0af19caa0f624c2375136dc4bb8cefa5b8
ST10225560 <ST10225560@vcconnect.edu.za>
2024-10-07 20:45:27 +0200

    initial commit

--- APDS7311_POE_Task2_MTM_Solutions_Gr2/frontend/src/components/payment/DeletePayment.js
+++ APDS7311_POE_Task2_MTM_Solutions_Gr2/frontend/src/components/payment/DeletePayment.js
@@ -3,42 +3,37 @@ import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
 
 function DeletePayment() {
-    const { id } = useParams();
-    const navigate = useNavigate();
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-    const deletePayment = async () => {
-        try{
-            const token = localStorage.getItem('token');
-            await axios.delete(`/api/${id}`, {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            });
-            navigate('/payment');
-        }
-        catch(err){
-            console.log("Error in deleting of payments: ", err);
-        }
+  const deletePayment = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      await axios.delete(`/api/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      navigate("/payment");
+    } catch (err) {
+      console.log("Error in deleting of payments: ", err);
     }
-    
-    return (
-        <div className="container">
-            <h1 className="title">Delete Payments</h1>
-            <p className="label">Are you sure you want to delete this payment?</p>
-            <div className="submit container">
-                <button 
-                onClick={deletePayment}
-                className="cancelbtn">
-                    Delete
-                </button>
-                <button 
-                onClick={() => navigate(-1)}
-                className="submitbtn">
-                    Cancel
-                </button>
-            </div>
-        </div>
-    );
+  };
+
+  return (
+    <div className="container">
+      <h1 className="title">Delete Payments</h1>
+      <p className="label">Are you sure you want to delete this payment?</p>
+      <div className="submit container">
+        <button onClick={deletePayment} className="cancelbtn">
+          Delete
+        </button>
+        <button onClick={() => navigate(-1)} className="submitbtn">
+          Cancel
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default DeletePayment;
